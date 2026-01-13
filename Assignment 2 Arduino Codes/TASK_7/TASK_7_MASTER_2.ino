ac40210eb6dc79e8f498b38d64427d4c80a80465
Saikiran-Guruprasad <saikiranpr@gmail.com>
2026-01-13 09:46:01 +0530

    Update TASK_7_MASTER_2.ino

--- Assignment 2 Arduino Codes/TASK_7/TASK_7_MASTER_2.ino
+++ Assignment 2 Arduino Codes/TASK_7/TASK_7_MASTER_2.ino
@@ -1,21 +1,81 @@
 #include <Wire.h>
 
-void setup(){
-  Wire.begin();       
-} 
+int c = 0, d = 0;
+int sum_cd = 0;
+bool shouldAcquireBus = false;
+bool readyToBroadcast = false;
 
-void loop(){
-  int c,d;
-  Wire.requestFrom(6, 2);   
-  if (Wire.available()){
-     c = Wire.read();
+void setup() {
+  Wire.begin(0x05);
+  Wire.onReceive(receiveFromM1);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (shouldAcquireBus) {
+    Wire.end();
+    Wire.begin();
+    delay(50);
+
+    Wire.requestFrom(0x06, 1);
+    if (Wire.available()) {
+      c = Wire.read();
+      Serial.print("Received c = ");
+      Serial.println(c);
+    }
+    delay(50);
+
+    Wire.requestFrom(0x07, 1);
+    if (Wire.available()) {
+      d = Wire.read();
+      Serial.print("Received d = ");
+      Serial.println(d);
+    }
+    delay(50);
+
+    sum_cd = c + d;
+    Serial.print("Computed (c+d) = ");
+    Serial.println(sum_cd);
+
+    Wire.beginTransmission(0x04);
+    Wire.write(1);
+    Wire.endTransmission();
+    delay(100);
+
+    Wire.end();
+    Wire.begin(0x05);
+    Wire.onReceive(receiveBroadcast);
+
+    shouldAcquireBus = false;
+    readyToBroadcast = true;
   }
-  Wire.requestFrom(7, 2);   
-  if (Wire.available()){
-    d = Wire.read();
+
+  delay(10);
+}
+
+void receiveFromM1(int numBytes) {
+  if (!readyToBroadcast) {
+    while (Wire.available()) {
+      Wire.read();
+    }
+    shouldAcquireBus = true;
   }
-  Wire.beginTransmission(0);
-  Wire.write(c+d);
-  Wire.endTransmission();
-  delay(500);
+}
+
+void receiveBroadcast(int numBytes) {
+  if (readyToBroadcast && Wire.available()) {
+    Wire.read();
+    Wire.end();
+    Wire.begin();
+    delay(50);
+    Wire.beginTransmission(0x00);
+    Wire.write(sum_cd);
+    Wire.endTransmission();
+    delay(100);
+
+    Wire.end();
+    Wire.begin(0x05);
+    Wire.onReceive(receiveFromM1);
+    readyToBroadcast = false;
+  }
 }
